4a75037abf2c01f44391f04c6b135a2af65838ee
sambit <sambitmahala653@gmail.com>
2022-08-22 12:40:05 +0530

    addWelcomePage

---
+++ src/components/Pages/WelcomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import'./WelcomePage.css'
+
+function WelcomePage() {
+  return (
+    <div>
+      <h1>WELLCOME TO EXPENXE TRACKER</h1>
+    </div>
+  )
+}
+
+export default WelcomePage
