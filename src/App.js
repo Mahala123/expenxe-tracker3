b558b0f9bacee7e26c0901e05eb281f4d388bfcf
sambit <sambitmahala653@gmail.com>
2022-08-22 12:31:57 +0530

    lgin button work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 //import logo from './logo.svg';
 import './App.css';
+import { useContext } from 'react';
 import SignUp from './components/signUp/SignUp';
+import{Route,Switch} from 'react-router-dom'
+import WelcomePage from './components/Pages/WelcomePage';
+import AuthContext from './components/Store/Store';
+ 
 
 
 function App() {
+  const authCtx=useContext(AuthContext)
+  const isLogIn=authCtx.isLogIn
   return (
     <div className="App">
+    <Switch>
+    {isLogIn && <Route path='/welcome'exact>
+      <WelcomePage/>
+    </Route>}
+  {!isLogIn && <Route path="/">
     <SignUp/>
-      
+    </Route> }
+    
+    </Switch> 
     </div>
   );
 }
