4c09818f9f16ea38680831e9439aba1f25df8a1e
sambit <sambitmahala653@gmail.com>
2022-08-23 18:03:34 +0530

    forgetpassword work

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import{Route,Switch} from 'react-router-dom'
 import WelcomePage from './components/Pages/WelcomePage';
 import AuthContext from './components/Store/Store';
 import CompleteProfile from './components/Pages/CompleteProfile';
+import ForgetPassword from './components/Pages/ForgetPassword';
  
 
 
@@ -21,10 +22,12 @@ function App() {
     <Route path='/completeProfile'>
       <CompleteProfile/>
     </Route>
+    <Route path='/password' exact>
+      <ForgetPassword/>
+    </Route>
   {!isLogIn && <Route path="/">
     <SignUp/>
     </Route> }
-    
     </Switch> 
     </div>
   );
