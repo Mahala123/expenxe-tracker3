fc8403819073fe225c266045182a2f4149af6e95
sambit <sambitmahala653@gmail.com>
2022-08-23 13:22:00 +0530

    add verifyEmail

--- src/components/Pages/WelcomePage.js
+++ src/components/Pages/WelcomePage.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import'./WelcomePage.css'
 import{NavLink} from 'react-router-dom'
+import AuthContext from '../Store/Store'
 
 function WelcomePage() {
+  const authCtx=useContext(AuthContext)
+  const verifyEmail=()=>{
+   fetch("https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAIpFpXqTkA-YkLV506rGbmUDu1_nJmw5I",
+   {
+    method: 'POST',
+        body:JSON.stringify({
+           
+        idToken:authCtx.token,
+        requestType:'VERIFY_EMAIL',
+          }),
+          headers:{
+            'Content-Type': 'application/json' 
+       }
+   }).then(res=>{
+    if(res.ok)
+    {
+        alert('email is varified');
+    }
+    else{
+        alert('aunthentication failed');
+    }
+   })
+  }
   return (
     <div className='comProfile'>
       <h3>WELLCOME TO EXPENXE TRACKER</h3>
       <div className='comProfile'>YOUR profile is incomplete <NavLink to='/CompleteProfile'>complete Now</NavLink></div>
+    <button onClick={verifyEmail}>VERIFY YOUR EMAIL</button>
     </div>
   )
 }
