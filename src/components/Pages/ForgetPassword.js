9685435895d81ea3b1039c5ad327409ea7ff9f75
sambit <sambitmahala653@gmail.com>
2022-08-23 18:02:18 +0530

    forget password work

---
+++ src/components/Pages/ForgetPassword.js
@@ -0,0 +1,46 @@
+import React,{useRef} from 'react'
+import{useHistory} from 'react-router-dom'
+
+function ForgetPassword() {
+  const inputEmail=useRef()
+  const history=useHistory()
+  const enterEmail=(event)=>{
+    event.preventDefault()
+   const email=inputEmail.current.value
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAIpFpXqTkA-YkLV506rGbmUDu1_nJmw5I',
+    {
+        method: 'POST',
+        body:JSON.stringify({
+            requestType:"PASSWORD_RESET",
+            email:email,
+          }),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+
+// 
+    })
+    .then(res=>{
+        if(res.ok)
+        {
+            alert('link sent to the entered email');
+            history.replace('/');
+           
+            }
+        }
+
+    )
+
+  }
+  return (
+    <div className='signupBody1'>
+    <h2>Entered Your Registered Email</h2>
+    <form onClick={enterEmail} >
+    <input type="email" placeholder='Email'ref={inputEmail}  required />
+    <button type='submit' className='signupBtn1' >SEND EMAIL</button>
+    </form>  
+    </div>
+  )
+}
+
+export default ForgetPassword
