a122810a260cb1e153db294d4f9cab518a382544
sambit <sambitmahala653@gmail.com>
2022-08-27 00:12:08 +0530

    add expenxe store redux

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -18,9 +18,23 @@ const authslice=createSlice({
       }
   }
 })
+const initialExpState = {
+  expenses: {},
+  totalExpense: 0,
+};
 
-
-
+const expSlice = createSlice({
+  name: "expenses",
+  initialState: initialExpState,
+  reducers: {
+    setExpenses(state, action) {
+      state.expenses = action.payload;
+    },
+    setTotalExpense(state, action){ 
+      state.totalExpense = action.payload;
+    }
+  },
+});
 
 const store=configureStore({
     reducer:{auth:authslice.reducer,exp:expSlice.reducer}
